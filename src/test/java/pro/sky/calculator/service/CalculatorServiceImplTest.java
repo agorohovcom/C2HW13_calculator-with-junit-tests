21706ba36b1963d8b3a8afc7c3ce896d0b1e44c9
agorohov <aleksandr.gorohov@gmail.com>
2024-05-17 18:17:13 +0300

    покрыл параметризованными тестами все методы калькулятора

--- src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
+++ src/test/java/pro/sky/calculator/service/CalculatorServiceImplTest.java
@@ -5,43 +5,39 @@ import pro.sky.calculator.exception.DivisionByZeroException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static pro.sky.calculator.constants.CalculatorServiceImplTestConstants.*;
 
 class CalculatorServiceImplTest {
 
     private final CalculatorServiceImpl out = new CalculatorServiceImpl();
 
-    private final int firstNum1 = 30;
-    private final int firstNum2 = 66;
-    private final int secondNum1 = -100500;
-    private final int secondNum2 = 300;
-
     @Test
     public void shouldCalculateSumCorrectly() {
-        assertEquals(out.plus(firstNum1, secondNum1), firstNum1 + secondNum1);
-        assertEquals(out.plus(firstNum2, secondNum2), firstNum2 + secondNum2);
+        assertEquals(out.plus(NUM_1, NUM_2), NUM_1 + NUM_2);
+        assertEquals(out.plus(NUM_3, NUM_4), NUM_3 + NUM_4);
     }
 
     @Test
     public void shouldCalculateMinusCorrectly() {
-        assertEquals(out.minus(firstNum1, secondNum1), firstNum1 - secondNum1);
-        assertEquals(out.minus(firstNum2, secondNum2), firstNum2 - secondNum2);
+        assertEquals(out.minus(NUM_1, NUM_2), NUM_1 - NUM_2);
+        assertEquals(out.minus(NUM_3, NUM_4), NUM_3 - NUM_4);
     }
 
     @Test
     public void shouldCalculateMultiplyCorrectly() {
-        assertEquals(out.multiply(firstNum1, secondNum1), firstNum1 * secondNum1);
-        assertEquals(out.multiply(firstNum2, secondNum2), firstNum2 * secondNum2);
+        assertEquals(out.multiply(NUM_1, NUM_2), NUM_1 * NUM_2);
+        assertEquals(out.multiply(NUM_3, NUM_4), NUM_3 * NUM_4);
     }
 
     @Test
     public void shouldCalculateDivideCorrectly() {
-        assertEquals(out.divide(firstNum1, secondNum1), firstNum1 / secondNum1);
-        assertEquals(out.divide(firstNum2, secondNum2), firstNum2 / secondNum2);
+        assertEquals(out.divide(NUM_1, NUM_2), NUM_1 / NUM_2);
+        assertEquals(out.divide(NUM_3, NUM_4), NUM_3 / NUM_4);
     }
 
     @Test
     public void shouldThrowsDivisionByZeroException() {
         assertThrows(DivisionByZeroException.class,
-                () -> out.divide(firstNum1, 0));
+                () -> out.divide(NUM_1, 0));
     }
 }
